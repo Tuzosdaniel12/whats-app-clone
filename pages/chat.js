35cb00d720eebf35c51590f75df12de6c95fc885
daniel <danielsoledad@gmail.com>
2021-07-06 19:05:09 -0700

    re did file structure

---
+++ pages/chat.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeadTab from '../components/Head'
+
+const chat = () => {
+    return (
+        <div>
+        <HeadTab/>
+        
+            Hello this is the chat!!!!
+        </div>
+    )
+}
+
+export default chat
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import styles from '../styles/Home.module.css'
+import Head from '../components/Head.js'
+
+export default function Home() {
+  return (
+		<div className={styles.container}>
+			<Head/>
+			
+			<main className={styles.main}>
+				
+			</main>
+		</div>
+  );
+}
