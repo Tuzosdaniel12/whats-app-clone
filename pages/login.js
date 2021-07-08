79cc72f680c5286d5bae12ade9fc2d0b8da9f708
daniel <danielsoledad@gmail.com>
2021-07-07 22:01:44 -0700

    removed padding from login

--- pages/login.js
+++ pages/login.js
@@ -12,7 +12,10 @@ const login = () => {
 			<HeadTab title="Login" />
 
 			<LoginContainer>
-				<Logo src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" alt="logo"/>
+				<Logo
+					src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+					alt="logo"
+				/>
 				<Button onClick={signIn} variant="outlined">
 					Sign in with Google
 				</Button>
@@ -27,6 +30,7 @@ const Container = styled.div`
 	display: grid;
 	place-items: center;
 	height: 100vh;
+	width: 100vw;
 	background-color: whitesmoke;
 `;
 
@@ -38,10 +42,15 @@ const LoginContainer = styled.div`
 	background-color: white;
 	border-radius: 5px;
 	box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.75);
+
+	@media (max-width: 425px) {
+		padding: 50px;
+	}
 `;
 
 const Logo = styled.img`
 	height: 200px;
 	width: 200px;
 	margin-bottom: 50px;
+
 `;
